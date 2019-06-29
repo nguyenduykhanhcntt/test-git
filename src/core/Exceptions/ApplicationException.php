aa10b2438b944fd1cc7527f0f3caeeb38d95db1b
Duy Khánh <nguyenduykhanhcntt92@gmail.com>
2019-06-29 15:01:06 +0700

    Revert " oke2"
    This reverts commit f8b990f2e0f70a26e62c58f5ef75ae2a456b716e.

--- src/core/Exceptions/ApplicationException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Core\Exceptions;
-
-
-class ApplicationException extends \Exception
-{
-
-}
--- src/public/exten-db2.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Molde extends Pdoo{
-    public function getAllBanhang(){
-        $sql = 'select * from ban_hang';
-        return $this->fetchall($sql);
-    }
-}
